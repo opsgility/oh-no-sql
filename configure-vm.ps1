9cecf0ebb5d71af34562673f227cbd098acbff36
Michael Washam <michael@opsgility.com>
2019-03-31 07:12:05 -0500

    initial commit vm extensions

---
+++ configure-vm.ps1
@@ -0,0 +1,92 @@
+﻿param($sourceFileUrl="", $destinationFolder="",$dbsource="", $databaseName="")
+$ErrorActionPreference = 'SilentlyContinue'
+
+if([string]::IsNullOrEmpty($sourceFileUrl) -eq $false -and [string]::IsNullOrEmpty($destinationFolder) -eq $false)
+{
+    if((Test-Path $destinationFolder) -eq $false)
+    {
+        New-Item -Path $destinationFolder -ItemType directory
+    }
+    $splitpath = $sourceFileUrl.Split("/")
+    $fileName = $splitpath[$splitpath.Length-1]
+    $destinationPath = Join-Path $destinationFolder $fileName
+
+    (New-Object Net.WebClient).DownloadFile($sourceFileUrl,$destinationPath);
+
+    (new-object -com shell.application).namespace($destinationFolder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
+}
+
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+
+# Install Chrome
+$Path = $env:TEMP; 
+$Installer = "chrome_installer.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
+
+# Install VS Code
+$Path = $env:TEMP; 
+$Installer = "vscode.exe"
+Invoke-WebRequest "https://go.microsoft.com/fwlink/?Linkid=852157" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/verysilent /MERGETASKS=!runcode" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
+
+# Install Azure CLI 2
+$Path = $env:TEMP; 
+$Installer = "cli_installer.msi"
+Write-Host "Downloading Azure CLI 2..." -ForegroundColor Green
+Invoke-WebRequest "https://aka.ms/InstallAzureCliWindows" -OutFile $Path\$Installer
+Write-Host "Installing Azure CLI from $Path\$Installer..." -ForegroundColor Green
+Start-Process -FilePath msiexec -Args "/i $Path\$Installer /quiet /qn /norestart" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
+
+# Install IIS and the Web App
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature
+
+$webAppUrl = ""
+$splitpath = $webAppUrl.Split("/")
+$fileName = $splitpath[$splitpath.Length-1]
+$destinationPath = "C:\Inetpub\wwwroot\ContosoJobs.zip"
+$destinationFolder = "C:\Inetpub\wwwroot"
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($cloudShopUrl,$destinationPath)
+
+#(new-object -com shell.application).namespace($destinationFolder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
+Expand-Archive -LiteralPath $destinationPath -DestinationPath $destinationFolder -Force
+
+# Deploy the DB
+$logs    = "C:\Logs"
+$data    = "C:\Data"
+$backups = "C:\Backup" 
+$script  = "C:\Script" 
+
+[system.io.directory]::CreateDirectory($logs)
+[system.io.directory]::CreateDirectory($data)
+[system.io.directory]::CreateDirectory($backups)
+[system.io.directory]::CreateDirectory($script)
+[system.io.directory]::CreateDirectory("C:\SQLDATA")
+
+$splitpath = $sqlConfigUrl.Split("/")
+$fileName = $splitpath[$splitpath.Length-1]
+$destinationPath = "$script\configure-sql.ps1"
+# Download config script
+(New-Object Net.WebClient).DownloadFile($sqlConfigUrl,$destinationPath);
+
+# Get the Adventure works database backup 
+$dbdestination = "C:\SQLDATA\$databaseName"
+Invoke-WebRequest $dbsource -OutFile $dbdestination
+
+$password =  ConvertTo-SecureString "$password" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$user", $password)
+
+Enable-PSRemoting -Force
+Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
+Invoke-Command -FilePath $destinationPath -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList "Password", $password
+Disable-PSRemoting -Force
+
+New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound -Protocol TCP -LocalPort 1433 -Action allow 
