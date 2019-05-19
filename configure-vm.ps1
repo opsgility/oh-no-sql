a67751469b3a6bc221bc46719478b6df42ad8177
Michael Washam <michael@opsgility.com>
2019-05-19 14:51:25 -0500

    script update

--- configure-vm.ps1
+++ configure-vm.ps1
@@ -50,9 +50,9 @@ $script  = "C:\Script"
 [system.io.directory]::CreateDirectory("C:\SQLDATA")
 
 
-$destinationPath = "$script\configure-sql.ps1"
+#$destinationPath = "$script\configure-sql.ps1"
 # Download config script
-(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/opsgility/oh-no-sql/master/configure-sql.ps1",$destinationPath);
+#(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/opsgility/oh-no-sql/master/configure-sql.ps1",$destinationPath);
 
 # Get the Adventure works database backup 
 $dbdestination = "C:\SQLDATA\$databaseName"
@@ -61,9 +61,37 @@ Invoke-WebRequest $dbsource -OutFile $dbdestination
 $password =  ConvertTo-SecureString "$password" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$user", $password)
 
+
+$configureSQLBlock =
+{
+
+    $dbdestination = "C:\SQLDATA\OpenHack.bak"
+    # Setup the data, backup and log directories as well as mixed mode authentication
+    Import-Module "sqlps" -DisableNameChecking
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+    $sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') Localhost
+    $sqlesq.Settings.LoginMode = [Microsoft.SqlServer.Management.Smo.ServerLoginMode]::Mixed
+    $sqlesq.Settings.DefaultFile = $data
+    $sqlesq.Settings.DefaultLog = $logs
+    $sqlesq.Settings.BackupDirectory = $backups
+    $sqlesq.Alter() 
+
+    # Re-enable the sa account and set a new password to enable login
+    Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa ENABLE" 
+
+    Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa WITH PASSWORD = '" + $args[0] + "'"
+
+
+    Restore-SqlDatabase -ServerInstance Localhost -Database "OpenHack" -BackupFile "C:\SQLDATA\OpenHack.bak"
+
+    # Restart the SQL Server service
+    Restart-Service -Name "MSSQLSERVER" -Force
+
+}
+
 Enable-PSRemoting -Force
 Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
-Invoke-Command -FilePath $destinationPath -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $password
+Invoke-Command -ScriptBlock $configureSQLBlock -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $password
 Disable-PSRemoting -Force
 
 New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound -Protocol TCP -LocalPort 1433 -Action allow 
